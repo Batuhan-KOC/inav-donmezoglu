7e659f903af3450c53bda42deae7c041a20ecdbf
Batuhan KOÇ <1997.batuhankoc@gmail.com>
2025-05-06 20:53:55 +0300

    Safety controls added to serial printing functions

--- src/main/fc/donmezoglu.c
+++ src/main/fc/donmezoglu.c
@@ -51,13 +51,17 @@ void donmezogluUpdate(timeUs_t currentTimeUs){
 }
 
 void donmezogluSerialPrintS(const char* str){
-    // Iterate through each character in the string until '\0' (null terminator) is encountered
-    for (int i = 0; str[i] != '\0'; i++) {
-        // Call serialWrite for each character in the string
-        donmezogluSerialPrintC((uint8_t)str[i]);
+    if(dSerialPort != NULL){
+        // Iterate through each character in the string until '\0' (null terminator) is encountered
+        for (int i = 0; str[i] != '\0'; i++) {
+            // Call serialWrite for each character in the string
+            donmezogluSerialPrintC((uint8_t)str[i]);
+        }
     }
 }
 
 void donmezogluSerialPrintC(uint8_t ch){
-    serialWrite(dSerialPort, ch);
+    if(dSerialPort != NULL){
+        serialWrite(dSerialPort, ch);
+    }
 }
